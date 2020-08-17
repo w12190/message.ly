6ecfca6792cd32ad2976e529654bc510d0881339
Wayne Chen <wayne12190@gmail.com>
2020-08-17 16:09:02 -0700

    added register(), authenticate(), and updateLoginTimestamp() in User class; added handle POST request to /login and /register

--- models/user.js
+++ models/user.js
@@ -1,30 +1,72 @@
 "use strict";
 
+const db = require('../db')
+const bcrypt = require('bcrypt');
+const { BCRYPT_WORK_FACTOR } = require('../config');
+
+
+
 /** User of the site. */
 
 class User {
-
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
-
   static async register({ username, password, first_name, last_name, phone }) {
+    //Calculates pw hash
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+    // debugger
+
+    //Insert into DB
+    const results = await db.query(`
+      INSERT INTO users (username,
+                         password,
+                         first_name,
+                         last_name,
+                         phone,
+                         join_at)
+      VALUES
+          ($1, $2, $3, $4, $5, $6)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone, new Date()])
+    return results.rows[0]
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
-
   static async authenticate(username, password) {
+    // Query DB for "username"
+    const results = db.query(`
+      SELECT username, password
+      FROM users
+      WHERE username = $1`, [username])
+
+    // Check if user exists
+    if (results.rows[0]){
+      // Check if password correct
+      if (await bcrypt.compare(password, results.rows[0][password]) === True){
+        return true
+      }
+    }
+    return false
   }
 
   /** Update last_login_at for user */
-
   static async updateLoginTimestamp(username) {
+    // Assume logged in via authenticate()
+    const results = db.query(`
+      UPDATE users
+      SET last_login_at = $1
+      WHERE username = $2
+      RETURNING last_login_at
+      `, [new Date(), username])
+    console.log(results.rows[0]['last_login_at'])
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    
   }
 
   /** Get: get user by username
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,52 @@
 "use strict";
 
+// Imports
+const User = require('../models/user')
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+const { UnauthorizedError } = require('../expressError')
+
 const Router = require("express").Router;
 const router = new Router();
 
-/** POST /login: {username, password} => {token} */
+// Middleware
+router.use(express.json());
 
+
+/** POST /login: {username, password} => {token} */
+router.post('/login', function (req, res, next) {
+  console.log('Route: /login')
+
+  try {
+    const { username, password } = req.body
+    // Check if user credentials valid
+    if (User.authenticate(req.body)) {
+      User.updateLoginTimestamp(username)
+
+      // Create and return JWT token
+      const token = jwt.sign({ username }, config.SECRET_KEY)
+      return res.json({ token })
+    }
+    else {
+      throw new UnauthorizedError()
+    }
+  }
+  catch (error) {
+    next(error)
+  }
+})
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post('/register', async function (req, res, next) {
+  console.log('Route: /register')
 
+  const newUser = await User.register(req.body)
+  return res.json(newUser)
+})
+
+// Exports
 module.exports = router;
